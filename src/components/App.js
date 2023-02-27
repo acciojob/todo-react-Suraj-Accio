21580a7e027bd7279826bc6ba51352bf6acff2f3
Suraj-Accio <124239927+Suraj-Accio@users.noreply.github.com>
2023-02-27 16:11:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,40 @@
 import React, { useState } from "react";
+import "./todolist.css";
 
 function App() {
- const [todos, setTodos] = useState([]);
- const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
 
- const handleAddTodo = () => {
-   setTodos([...todos, newTodo]);
-   setNewTodo("");
- };
+  const handleAddTodo = () => {
+    setTodos([...todos, newTodo]);
+    setNewTodo("");
+  };
 
- const handleRemoveTodo = (index) => {
-   setTodos(todos.filter((_, i) => i !== index));
- };
+  const handleRemoveTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
 
- return (
-   <div>
-     <p>To-Do List:</p>
-     <ul>
-       {todos.map((todo, index) => (
-         <li key={index}>
-           {todo}
-           <button onClick={() => handleRemoveTodo(index)}>x</button>
-         </li>
-       ))}
-     </ul>
-     <input
-       type="text"
-       value={newTodo}
-       onChange={(e) => setNewTodo(e.target.value)}
-     />
-     <button onClick={handleAddTodo}>Add Todo</button>
-   </div>
- );
+  return (
+    <div>
+      <p>To-Do List:</p>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <div className="item">
+            <li key={index}>
+              {todo}
+            </li>
+            <button onClick={() => handleRemoveTodo(index)}>Delete</button>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
-
